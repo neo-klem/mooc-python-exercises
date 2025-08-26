4d95df5bcb5c665e344fd296d3b459b9b545357f
neo-klem <servisracunaramaya@gmail.com>
2025-08-26 20:15:44 +0200

    New additions

---
+++ part-1/Name twice.py
@@ -0,0 +1,13 @@
+#Please write a program which asks for the user's name and then prints it twice, on two consecutive lines.
+
+name = input("What is your name? ")
+
+print(f"{name}\n{name}")
+
+"""
+
+Here we use \n for new row so we don't repeat the print statement
+The same can be achived with using print() twice with name variable but it's repetitive
+Don't repeat yourself!
+
+"""
