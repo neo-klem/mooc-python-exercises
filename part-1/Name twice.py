35640db76e412bc2cfc13ff2a86e72a9ba68ab64
neo-klem <servisracunaramaya@gmail.com>
2025-08-26 20:30:40 +0200

    Newest additions

--- part-1/Name twice.py
+++ part-1/Name twice.py
@@ -7,7 +7,11 @@ print(f"{name}\n{name}")
 """
 
 Here we use \n for new row so we don't repeat the print statement
+
 The same can be achived with using print() twice with name variable but it's repetitive
 Don't repeat yourself!
+
+We also use something called f-string - notice the f before starting with quotation marks
+We use {} to put our variables inside placeholders
 
 """
