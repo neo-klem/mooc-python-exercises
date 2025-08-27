953f380825d763e9f730a63baefd18dda8e649dc
neo-klem <servisracunaramaya@gmail.com>
2025-08-27 19:50:30 +0200

    Latest

---
+++ part-1/Fix the code-Seven Brothers.py
@@ -0,0 +1,23 @@
+"""
+
+This program is supposed to print out 
+the names of the brothers in alphabetical order,
+but it's not working quite right yet. 
+
+Please fix the program so that the 
+names are printed in the correct order.
+
+
+"""
+
+
+
+
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
