4d075db055656becf18d33c2c51b64b09022387c
neo-klem <servisracunaramaya@gmail.com>
2025-08-29 18:20:55 +0200

    Updates

---
+++ part-1/Sum and product.py
@@ -0,0 +1,13 @@
+# Ask for two numbers
+# Print their sum and product
+
+num_1 = int(input("Number 1: "))
+num_2 = int(input("Number 2: "))
+sum = num_1 + num_2
+product = num_1 * num_2
+
+print(f"The sum of the numbers: {sum}")
+print(f"The product of the numbers: {product}")
+
+# It's always better to store such info inside a variable
+
